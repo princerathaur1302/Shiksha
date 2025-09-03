474922dc464a96de60201d93362c053db101ab38
princerathaur1302 <princerathaur1302@gmail.com>
2025-09-03 21:56:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key-here'
+    SECRET_KEY = 'a3f91b56c7d43ba37gsf2cd27fs909e1c9d53f8a8723e4ff9d1c8e29f3cfd123456789ab'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///siksha_academy.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
